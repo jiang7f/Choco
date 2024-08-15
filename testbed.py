4086b845e31d51fae01b4648043eec917f0035c3
jqf <jiang7ff@163.com>
2024-08-15 20:58:23 +0800

    lcbo 0.5

--- testbed.py
+++ testbed.py
@@ -1,14 +1,15 @@
 from Choco.model.lin_constr_bin_opt import LinearConstrainedBinaryOptimization as Model
 if __name__ == '__main__':
     m = Model()
-    num_facilities = 1
-    num_demands = 1
+    num_1 = 1
+    num_2 = 1
     m.addVar(name='jqf')
-    x = m.addVars(num_facilities, name="x")
-    y = m.addVars(num_demands, num_facilities, name="y")
-    m.setObjective(sum(3 * y[i, j] for i in range(num_demands) for j in range(num_facilities)) + sum(4 * x[j] for j in range(num_facilities)), 'min')
 
-    m.addConstrs((x[j] >= 1 for i in range(num_demands) for j in range(num_facilities)))
+    x = m.addVars(num_1, name="x")
+    y = m.addVars(num_2, num_1, name="y")
+    m.setObjective(sum(3 * y[i, j] for i in range(num_2) for j in range(num_1)) + sum(4 * x[j] for j in range(num_1)), 'min')
+
+    m.addConstrs((x[j] >= 1 for i in range(num_2) for j in range(num_1)))
     obj = m.generate_obj_function()
     print(m)
     print(obj([1] * len(m.variables)))
